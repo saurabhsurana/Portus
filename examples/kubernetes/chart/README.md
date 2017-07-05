5b47981a805c1f4c5aba261bed2bd6eb68498fab
Saurabh Surana <saurabh@registry.mssola.cat>
2017-07-05 13:52:11 -0700

    kubernetes chart for deploying Portus

---
+++ examples/kubernetes/chart/README.md
@@ -0,0 +1,3 @@
+# Portus
+
+Portus is an authorization server and a user interface for the next generation of the Docker registry. Portus targets version 2 of the Docker Registry API. The minimum required version of Registry is 2.1, which is the first version supporting soft deletes of blobs.
--- examples/kubernetes/create-secrets-from-files.sh
+++ examples/kubernetes/create-secrets-from-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+NAMESPACE=${NAMESPACE:-default}
+
 # Certificates
-kubectl create secret generic certificates --from-file=portus.key=./certificates/portus.key --from-file=portus.crt=./certificates/portus.crt
+kubectl create secret generic certificates --from-file=portus.key=./certificates/portus.key --from-file=portus.crt=./certificates/portus.crt --namespace $NAMESPACE
 
 # Registry configuration
-kubectl create secret generic registry-config --from-file=config.yml=./registry/config.yml --from-file=init=./registry/init
+kubectl create secret generic registry-config --from-file=config.yml=./registry/config.yml --from-file=init=./registry/init --namespace $NAMESPACE
